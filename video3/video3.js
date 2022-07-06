25ec4df16defa624cba6e8607670dad92074d975
Voanhnguyen <voanhnguyenxd@gmail.com>
2022-07-06 09:55:43 +0700

    chapter 3, video3

--- video3/video3.js
+++ video3/video3.js
@@ -4,3 +4,6 @@ let x=5, y=6, z=7;
 
 console.log("test string: ", 'x= ',x, 'y=', y, 'z= ', z)
 
+console.log("test String 2222:", +'x = ' + x + 'y= ' + y +'z= ' +z)
+
+console.log("test String 2222:", 'x = ' + 5 + 'y= ' + 6 + 'z= ' +7 )
