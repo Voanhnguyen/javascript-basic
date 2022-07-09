cc4684d7215020920d340f80a23176dfa0b48449
Macbook <macbook@Vo-Nguyen.local>
2022-07-09 21:49:29 +0700

    video1,2,3,4

---
+++ video4/video4.js
@@ -0,0 +1,8 @@
+let a = `We are the so-caller "Vikings" from the north`
+let text = `I'm a string`
+
+
+console.log(a)
+console.log(text)
+console.log(4 + 5 + 'a') // = 9 + 'a' = 9a
+console.log('a' + 4 + 5) // = a5 + 4  = a54
