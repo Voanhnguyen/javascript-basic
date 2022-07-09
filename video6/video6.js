8aa0ca3197f34ea8ebc750f86455f311de2322a8
Voanhnguyen <73056780+Voanhnguyen@users.noreply.github.com>
2022-07-09 23:00:45 +0700

    Update video6.js

--- video6/video6.js
+++ video6/video6.js
@@ -4,6 +4,6 @@ let a = 5; //int,float,long,double
 let b = 6;
 let c = 6.9;
 let d = '5'
-let e = +d;
+let e = +d; //convert string sang number
 console.log('e value:', e, 'type:', typeof e)
-console.log(a===e,'a:', typeof a, 'd:', typeof e)
+console.log(a===e,'a:', typeof a, 'd:', typeof e) //trước khi so sánh phải convert về số rồi mới so sánh
