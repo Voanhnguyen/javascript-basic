2bac5f50d97b3c86d2251f0cf868b4bf7aaf01ca
Macbook <macbook@Vo-Nguyen.local>
2022-07-10 16:02:43 +0700

    template strings

---
+++ video7/video7.js
@@ -0,0 +1,9 @@
+console.log('Hello world from HTML')
+
+let a = 5;
+let b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+console.log('Fifteen is ' + a + b + ' and\nnot ' + (2 * a + b) + '.');
+
+
+console.log(`Fifteen is  ${a + b}   and\nnot  + ${2 * a + b}.`);
