eb7307cc1d278fcc38012c3ef1b5355f7e58a59e
Macbook <macbook@Vo-Nguyen.local>
2022-07-10 21:57:54 +0700

    Objects

---
+++ video8/video8.js
@@ -0,0 +1,18 @@
+console.log('Hello world from HTML')
+
+// key: value
+let obj = {
+    name: 'Eric',
+    address: 'Ha Noi', 
+    email: '',
+    gender: '',
+}; 
+
+let b = 'address';
+// obj[b] = 'Quang Tri';
+obj.address ='HCM'
+
+console.log('what is your name ? ', `My name's: `, obj.name)
+console.log('where are you from ? ', `I'm': `, obj['address'])
+
+
