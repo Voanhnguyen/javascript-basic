8761cf9bd2087c118e222070aeab540fd4ecb0a6
Macbook <macbook@Vo-Nguyen.local>
2022-07-10 22:46:35 +0700

    Arrays

---
+++ video9/video9.js
@@ -0,0 +1,26 @@
+console.log('Hello world from HTML')
+
+let a = {
+    name: 'Nguyen',
+    age: 25
+}; //key: value
+
+console.log('check data object:', a)
+console.log('My name\'s : ', a.name)
+
+// Khai báo Array
+let b = ["Mu","Liver","Chelsea","ManCity"]
+console.log('type of: ',typeof b, b) // lấy toàn bộ array
+console.log('first item:', b[0], 'second item:', b[1]); // lấy từng giá trị của array
+
+let c = [{
+        name: 'Nguyen',
+        age: 25
+    },
+    {
+        name: 'Nhu',
+        age: 25
+    }
+];
+
+console.log('my famyly:', c)
