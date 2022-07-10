d9e36ad66d6d7ca21e62226492f13be01ee76965
Voanhnguyen <73056780+Voanhnguyen@users.noreply.github.com>
2022-07-10 16:28:56 +0700

    Update video7.js
    template strings

--- video7/video7.js
+++ video7/video7.js
@@ -3,7 +3,7 @@ console.log('Hello world from HTML')
 let a = 5;
 let b = 10;
 console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
-console.log('Fifteen is ' + a + b + ' and\nnot ' + (2 * a + b) + '.');
+console.log('Fifteen is ' + a + b + ' and\nnot ' + (2 * a + b) + '.'); //Thổ dân
 
 
-console.log(`Fifteen is  ${a + b}   and\nnot  + ${2 * a + b}.`);
+console.log(`Fifteen is  ${a + b}   and\nnot  + ${2 * a + b}.`); // template string (`.....{ biểu thức}..`}
