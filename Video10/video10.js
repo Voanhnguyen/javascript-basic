3ffa6b9428338b72338e4a9eb8bcf8aadd32c259
Macbook <macbook@Vo-Nguyen.local>
2022-07-12 00:23:58 +0700

    ==,===,null,empty,underfinde

---
+++ Video10/video10.js
@@ -0,0 +1,19 @@
+console.log('Hello world from HTML')
+
+let a = 5; // Number
+let b = '5'; // String
+
+// Empty null, undefined
+let c; // underfined -> Đã tạo biến nhưng chưa gán giá trị cho nó
+console.log('check c: ',c)
+
+let d = null;
+d = 5;
+d = ['Nguyen'];
+
+console.log('Check d: ', d)
+
+console.log('compare a == b : ', 'a = ', a, 'b = ', b, 'result: ', a == b)
+console.log('compare a === b : ', 'a = ', a, 'b = ', b, 'result: ', a === b)
+// === type,value (so sánh kiểu dữ liệu, giá trị)
+// == value (so sánh giá trị)
