2ff5248a4bd224aec555c764200c08eb968233af
Macbook <macbook@Vo-Nguyen.local>
2022-07-13 20:50:41 +0700

    for loops

---
+++ video11/video11.js
@@ -0,0 +1,11 @@
+console.log('Hello world form HTML')
+
+let arr = ['Mu','ManCity','Liver','Chelsea']
+let i = 0;
+for(let i = 0; i < arr.length; i++){
+    console.log('check variale: i = ', arr[i])
+}
+
+console.log('check variable i: ', i)
+
+ 
