1eb06c36a913f75f6dd977cc2cc0eed76dfd8548
Macbook <macbook@Vo-Nguyen.local>
2022-07-14 01:00:51 +0700

    variables & Block scope

---
+++ video15/video15.js
@@ -0,0 +1,16 @@
+console.log('Hello world from HTML')
+
+//global (toàn cục)
+//block (cục bộ) -> khai báo biến let ( khai báo ở đâu sử dụng ở đấy)
+
+// console.log('value x: ' , x)
+// let x = 5;
+
+for (let i = 0; i < 10 ; i++){
+    let a = 10;
+    if{a < 10}{
+        let b = 5;
+        console.log(a)
+    }
+    console.log('>>>> check value i : ', i)
+}
