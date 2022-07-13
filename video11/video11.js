ecd82dc86234f24388bb5d41469d4ce15a77bca3
Voanhnguyen <73056780+Voanhnguyen@users.noreply.github.com>
2022-07-13 20:58:22 +0700

    for loops

--- video11/video11.js
+++ video11/video11.js
@@ -6,6 +6,10 @@ for(let i = 0; i < arr.length; i++){
     console.log('check variale: i = ', arr[i])
 }
 
-console.log('check variable i: ', i)
+console.log('check variable i: ', i)// biến = 0
 
- 
+ // var vs let
+ /*
+    var phạm vi sử dụng rộng hơn
+    let khai báo trong phạm vi 1 hàm thì nó chỉ có tác dụng trong hàm đấy thôi
+ */
