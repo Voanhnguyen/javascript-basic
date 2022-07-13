cc86ce2890ad889883fb52cdecd7c23454d8a791
Macbook <macbook@Vo-Nguyen.local>
2022-07-13 23:50:20 +0700

    if/else statement, Break & Continue

---
+++ video13/video13.js
@@ -0,0 +1,13 @@
+console.log('Hello word form HTML')
+
+let arrTop4 = ['Live', 'Chelsea', 'Mu', 'Man City'];
+let i = 0;
+while (i < arrTop4.length){
+    i++;
+    if(arrTop4[i] ==='Chelsea'){
+        console.log('Found it: ', arrTop4[i])
+        continue;
+    }
+    console.log('>>> check j: ', i)
+    
+}
