238e0f3e6a7b58fda645642823b4ed3dec9c1c0a
Macbook <macbook@Vo-Nguyen.local>
2022-07-16 23:11:15 +0700

    Arrow Function

---
+++ video18/video18.js
@@ -0,0 +1,12 @@
+console.log('Hello world from HTML')
+
+// function getSum (a, b){
+//     return a + b;
+// }
+
+//arow function
+let sum = (a,b) => {
+    return a + b;
+}
+
+console.log(' check sum: ', sum(6,9))
