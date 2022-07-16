3095abcc7894f13b96ad184c0a0e1ebb77577405
Macbook <macbook@Vo-Nguyen.local>
2022-07-16 23:49:09 +0700

    Function vs Method

---
+++ video19/video19.js
@@ -0,0 +1,19 @@
+console.log('Hello world form HTML')
+
+let sum = (a,b) => {
+    return a + b;
+}
+
+console.log(' check sum: 9 + 6 = ', sum(6,9))
+
+let obj = {
+    name: 'Eric',
+    address: 'Ha Noi',
+    getName: function(){
+        return this.name
+    }
+}
+
+console.log(' >>> get name obj: ', obj.getName())
+
+//Function vs method => tái sử dụng code ( viết 1 lần sử dụng nhiều nơi)
