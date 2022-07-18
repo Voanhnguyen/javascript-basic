13a793d6f1ecf973876f40418f6ff2f4cf0e5842
Voanhnguyen <73056780+Voanhnguyen@users.noreply.github.com>
2022-07-18 21:36:10 +0700

    Arrow Function

--- video18/video18.js
+++ video18/video18.js
@@ -4,9 +4,9 @@ console.log('Hello world from HTML')
 //     return a + b;
 // }
 
-//arow function
+//arrow function
 let sum = (a,b) => {
     return a + b;
 }
-
+//
 console.log(' check sum: ', sum(6,9 ))
