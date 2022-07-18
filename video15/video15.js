9f65f769576bcc4d65a775410df541792efc3297
Voanhnguyen <73056780+Voanhnguyen@users.noreply.github.com>
2022-07-18 21:37:22 +0700

    Global , Block scope

--- video15/video15.js
+++ video15/video15.js
@@ -1,7 +1,7 @@
 console.log('Hello world from HTML')
 
 //global (toàn cục)
-//block (cục bộ) -> khai báo biến let ( khai báo ở đâu sử dụng ở đấy)
+//block (cục bộ) -> khai báo biến let ( khai báo ở đâu sử dụng ở đấy).
 
 // console.log('value x: ' , x)
 // let x = 5;
